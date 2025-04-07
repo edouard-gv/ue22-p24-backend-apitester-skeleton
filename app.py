77a413ffdaea6f32f78f1a401e0fa17f724acf25
Édouard <edouard@gomez-vaez.com>
2025-04-07 17:09:37 +0200

    Proposition de solution

--- app.py
+++ app.py
@@ -10,15 +10,51 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-data = pl.Path(__file__).parent.absolute() / 'data'
+data = pl.Path(__file__).parent.absolute() / "data"
 
 # Charger les données CSV
-associations_df = pd.read_csv(data / 'associations_etudiantes.csv')
-evenements_df = pd.read_csv(data / 'evenements_associations.csv')
-
-## Vous devez ajouter les routes ici : 
+associations_df = pd.read_csv(data / "associations_etudiantes.csv")
+evenements_df = pd.read_csv(data / "evenements_associations.csv")
 
 
+@app.route("/api/alive", methods=["GET"])
+def is_alive():
+    return {"message": "Alive"}, 200
 
-if __name__ == '__main__':
+
+@app.route("/api/associations", methods=["GET"])
+def associations():
+    return associations_df["id"].transpose().to_list()
+
+
+@app.route("/api/association/<int:id>", methods=["GET"])
+def association(id):
+    return associations_df[associations_df.id == id].transpose()[0].to_json()
+
+
+@app.route("/api/evenements", methods=["GET"])
+def evenements():
+    return evenements_df["id"].transpose().to_list()
+
+
+@app.route("/api/evenement/<int:id>", methods=["GET"])
+def evenement(id):
+    return evenements_df[evenements_df.id == id].transpose()[0].to_json()
+
+
+@app.route("/api/association/<int:id>/evenements", methods=["GET"])
+def association_evenements(id):
+    return list(
+        evenements_df[evenements_df.association_id == id].transpose().to_dict().values()
+    )
+
+
+@app.route("/api/association/type/<type>", methods=["GET"])
+def association_par_type(type):
+    return list(
+        associations_df[associations_df.type == type].transpose().to_dict().values()
+    )
+
+
+if __name__ == "__main__":
     app.run(debug=False)
